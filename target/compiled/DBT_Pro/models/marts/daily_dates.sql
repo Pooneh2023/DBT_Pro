6a959279b8277baa969a896f1fbc0ad8a2060023
Pooneh Nezam <pooneh_nezam@MacBook-Air-2.local>
2025-10-17 13:42:10 +0200

    Add .gitignore and remove unwanted files

--- target/compiled/DBT_Pro/models/marts/daily_dates.sql
+++
@@ -1,3 +0,0 @@
-
-select generate_series::date as day
-from generate_series(current_date - interval '6 day', current_date, interval '1 day')
--- target/run/DBT_Pro/models/hello_world.sql
+++
@@ -1,7 +0,0 @@
-
-  create view "postgres"."Data_Marketing"."hello_world__dbt_tmp"
-    
-    
-  as (
-    select 1 as id, current_date as built_on
-  );
